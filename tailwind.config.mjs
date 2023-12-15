9e9ff08f7cf0decc70b79f28913648e41714af34
Ivan <ivanimbelloni@gmail.com>
2023-12-15 00:18:06 -0300

    portfolio Astro 🚀

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,23 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'west-side': {
+				'50': '#fff9ed',
+				'100': '#fff2d4',
+				'200': '#ffe2a8',
+				'300': '#ffcc70',
+				'400': '#ffaa37',
+				'500': '#ff941a',
+				'600': '#f07306',
+				'700': '#c75707',
+				'800': '#9e440e',
+				'900': '#7f3a0f',
+				'950': '#451b05',
+			},
+			
+			
+		}
 	},
 	plugins: [],
 }
