0b50112589cf2bc26b87fad4d2bf77e2b6c21c86
Ivan <ivanimbelloni@gmail.com>
2024-03-20 18:44:59 -0300

    arreglos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,48 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+	default: flattenColorPalette,
+  } = require("tailwindcss/lib/util/flattenColorPalette");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 		colors: {
-			'west-side': {
-				'50': '#fff9ed',
-				'100': '#fff2d4',
-				'200': '#ffe2a8',
-				'300': '#ffcc70',
-				'400': '#ffaa37',
-				'500': '#ff941a',
-				'600': '#f07306',
-				'700': '#c75707',
-				'800': '#9e440e',
-				'900': '#7f3a0f',
-				'950': '#451b05',
+			'jordy-blue': {
+				'50': '#f1f5fd',
+				'100': '#dfe9fa',
+				'200': '#c6d9f7',
+				'300': '#8bb4ee',
+				'400': '#72a0e8',
+				'500': '#507fe1',
+				'600': '#3c62d4',
+				'700': '#3250c3',
+				'800': '#2e429f',
+				'900': '#2a3b7e',
+				'950': '#1e264d',
 			},
+			
 			
 			
 		}
 	},
-	plugins: [],
+	plugins: [
+		addVariablesForColors,
+	],
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+   
+	addBase({
+	  ":root": newVars,
+	});
+
+  addBase({
+    ":root": newVars,
+  });
 }
